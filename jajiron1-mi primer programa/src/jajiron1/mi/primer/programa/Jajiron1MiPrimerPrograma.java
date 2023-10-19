b5f81a396cb2cde68b5103937f5b95b1a66a9ee1
Alejandro-Jiron <jajiron1@utpl.edu.ec>
2023-10-19 17:27:38 -0500

    mi primer prorama

---
+++ jajiron1-mi primer programa/src/jajiron1/mi/primer/programa/Jajiron1MiPrimerPrograma.java
@@ -0,0 +1,7 @@
+package jajiron1.mi.primer.programa;
+public class Jajiron1MiPrimerPrograma {
+    public static void main(String[] args) {
+        System.out.println("soy alejandro jiron");
+    }
+    
+}
